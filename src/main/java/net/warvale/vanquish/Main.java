a8b55228cd1439a72d58fa6b3fc83ed9765919e9
AAces <djp0908@yahoo.com>
2017-07-08 11:06:21 -0600

    Setup commands, and added one

--- src/main/java/net/warvale/vanquish/Main.java
+++ src/main/java/net/warvale/vanquish/Main.java
@@ -1,11 +1,14 @@
 package main.java.net.warvale.vanquish;
 
+import main.java.net.warvale.vanquish.commands.CommandHandler;
 import main.java.net.warvale.vanquish.listeners.BlockListener;
 import main.java.net.warvale.vanquish.listeners.PlayerListener;
 import main.java.net.warvale.vanquish.physics.ObsidianToLava;
+import main.java.net.warvale.vanquish.utils.Broadcast;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -13,19 +16,33 @@ import java.util.logging.Logger;
  */
 public class Main extends JavaPlugin {
     private static Main instance;
-
+    private static CommandHandler cmds;
     @Override
     public void onEnable(){
         instance = this;
+        try {
+            initialise();
+            Broadcast.toConsole(Level.INFO, "Successfully enabled!");
+        } catch(Exception ex) {
+            ex.printStackTrace();
+            getLogger().log(Level.WARNING, "Failed to enable LobbyCore!");
+        }
         Bukkit.getPluginManager().registerEvents(new BlockListener(), this);
         Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
-        ObsidianToLava.setDelay(5);
-        new ObsidianToLava().runTaskTimer(this, 0, 20);
-        getLogger().info("Enabled Vanquish");
     }
     @Override
     public void onDisable(){
         getLogger().info("Disabled Vanquish");
+    }
+
+    private void initialise(){
+
+        cmds = new CommandHandler(this);
+        cmds.registerCommands();
+
+        ObsidianToLava.setDelay(5);
+        new ObsidianToLava().runTaskTimer(this, 0, 20);
+
     }
 
     public static Main get(){
---
+++ src/main/java/net/warvale/vanquish/exceptions/CommandException.java
@@ -0,0 +1,13 @@
+package main.java.net.warvale.vanquish.exceptions;
+
+public class CommandException extends Exception {
+
+    /**
+     * Create a new CommandException
+     *
+     * @param message The error message.
+     */
+    public CommandException(String message) {
+        super(message);
+    }
+}
